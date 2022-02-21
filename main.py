1d343f2efbead90593fffa1ae4d6e8387ae443ea
Andresa Valerio <andresalvalerio@gmail.com>
2022-02-21 10:40:32 -0300

    wip: update main

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
 import pandas as pd
+import assets
 
-# TODO: Patrimônio Mínimo de Sobrevivência
-def pms(month_spend):
-    return 6 * month_spend
+print("Financial Organization")
+print("---")
 
-
-# TODO: Patrimônio Mínimo Recomendado
-def pmr(month_spend, employability):
-    if employability == "low":
-        return 12 * month_spend
-    elif employability == "high":
-        return 20 * month_spend
-
-
-# TODO: Patrimônio Ideal para a sua idade até a aposentadoria
-def pi(year_spend, age):
-    return  0,1 * year_spend * age
-
-
-# TODO: Patrimônio Necessário para Independência Financeira
-def pnif(year_spend, net_profitability):
-    return year_spend / net_profitability
-
+name = input("How can I call you? ")
 age = input("How old are you? ")
 retirement = input("How old will you be when you retire? ")
 time_to_retirement = retirement - age
@@ -30,5 +13,7 @@ month_spend = input("What is your average monthly spend? ")
 months_of_year = 12
 year_spend = month_spend * months_of_year
 total_investments = input("What is the total value of your investments? ")
-net_profitability = input("What is your average net profitability of your investments? "
+net_profitability = input("What is your average net profitability of your investments? ")
 employability = input("How good do you consider your emploiability?(high/low) ")
+
+user = assets.User(name, age)
